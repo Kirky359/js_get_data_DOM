9d0b9e6803b989b6c2bd4d8d6ecb0ae4089d7750
kirky359 <lol783176@gmail.com>
2025-08-26 18:04:55 +0300

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('.population');
+const arrayOfPopulation = Array.from(population).map((el) =>
+  Number(el.textContent.replaceAll(',', '')),);
+const total = arrayOfPopulation.reduce((sum, curr) => sum + curr, 0);
+const avg = total / arrayOfPopulation.length;
+
+const formattedTotal = total.toLocaleString('en-US');
+const formattedAvg = Math.round(avg).toLocaleString('en-US');
+
+document.querySelector(`.total-population`).textContent = formattedTotal;
+document.querySelector(`.average-population`).textContent = formattedAvg;
